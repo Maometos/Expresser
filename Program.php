5b438dcac69aeb9ff7d876fc0b11c90b5d7761ff
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-10-22 17:29:33 -0400

    initialize DevNet Web MVC project

---
+++ Program.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Application;
+
+use DevNet\Core\Hosting\WebHost;
+use DevNet\Core\Hosting\IWebHostBuilder;
+
+class Program
+{
+    public static function main(array $args = [])
+    {
+        self::createWebHostBuilder($args)->build()->run();
+    }
+
+    public static function createWebHostBuilder(array $args): IWebHostBuilder
+    {
+        return WebHost::createBuilder($args)
+            ->useStartup(Startup::class);
+    }
+}
