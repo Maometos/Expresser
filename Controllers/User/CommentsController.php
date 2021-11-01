73f32670c644e5398443a3528fea8ba17ee583bf
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-11-01 13:53:35 -0400

    add user comments page

---
+++ Controllers/User/CommentsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Application\Controllers\User;
+
+use DevNet\Core\Controller\AbstractController;
+use DevNet\Core\Controller\IActionResult;
+use DevNet\Core\Controller\Filters\AuthorizeFilter;
+
+class CommentsController extends AbstractController
+{
+    public function __construct()
+    {
+        $this->filter(self::class, AuthorizeFilter::class);
+    }
+
+    public function index() : IActionResult
+    {
+        return $this->view();
+    }
+}
