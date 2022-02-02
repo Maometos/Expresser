d7aac4055c42eba45717ea98ab855c358f58eef0
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-02-01 22:54:58 -0500

    create user/likes page

---
+++ Controllers/User/LikesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Application\Controllers\User;
+
+use DevNet\Web\Mvc\Controller;
+use DevNet\Web\Mvc\IActionResult;
+use DevNet\Web\Mvc\Filters\AuthorizeFilter;
+
+class LikesController extends Controller
+{
+    public function __construct()
+    {
+        $this->filter(self::class, AuthorizeFilter::class);
+    }
+
+    public function index() : IActionResult
+    {
+        return $this->view();
+    }
+}
