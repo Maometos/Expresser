95295a7fb4646f998bb7690f2fa7ece82ba53181
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-02-25 00:18:22 -0500

    rename LoginForm model to Login

--- Controllers/User/AccountController.php
+++ Controllers/User/AccountController.php
@@ -13,7 +13,7 @@ use DevNet\Web\Security\ClaimsPrincipal;
 use DevNet\Web\Security\ClaimsIdentity;
 use DevNet\Web\Security\ClaimType;
 use DevNet\Web\Security\Claim;
-use Application\Models\LoginForm;
+use Application\Models\Login;
 use Application\Models\Registration;
 use Application\Models\User;
 
@@ -46,7 +46,7 @@ class AccountController extends ActionController
         return $this->view();
     }
 
-    public function login(LoginForm $form): IActionResult
+    public function login(Login $form): IActionResult
     {
         $user = $this->HttpContext->User;
 
