03e669fc310b0ad3429e3620c278059775226613
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2023-01-23 01:50:42 -0500

    update user controllers extends AbstractController remove action filter from constructor

--- Controllers/User/PostsController.php
+++ Controllers/User/PostsController.php
@@ -2,17 +2,11 @@
 
 namespace Application\Controllers\User;
 
-use DevNet\Web\Controller\ActionController;
+use DevNet\Web\Controller\AbstractController;
 use DevNet\Web\Controller\IActionResult;
-use DevNet\Web\Controller\Filters\AuthorizeFilter;
 
-class PostsController extends ActionController
+class PostsController extends AbstractController
 {
-    public function __construct()
-    {
-        $this->filter(self::class, AuthorizeFilter::class);
-    }
-
     public function index() : IActionResult
     {
         return $this->view();
